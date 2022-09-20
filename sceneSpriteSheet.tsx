54bb4d606eeed9e7887d8f16ec2ff5e2c10b0c68
LoosAP <loosandraspeter@gmail.com>
2022-09-20 17:05:41 +0200

    FIRST commit

---
+++ sceneSpriteSheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.1" name="sceneSpriteSheet" tilewidth="12" tileheight="12" tilecount="1024" columns="32">
+ <image source="../../Downloads/sceneSpriteSheet.png" trans="ff00ff" width="384" height="384"/>
+</tileset>
